eb3fa1ebb1d550d92abba94c5314b3c87e94bd00
Amber <akansf1@gmail.com>
2025-06-17 16:47:31 -0500

    added nav.js - attempting to find an efficient way to include the navbar programmatically without using a framework.

--- nav.js
+++ nav.js
@@ -1,21 +1,40 @@
-const menu = document.getElementById("menu");
-const nav = document.getElementById("nav");
-console.log("Connected");
+document.getElementById('nav-wrapper').innerHTML(
+    `<nav class="nav" id="nav">
+        <ul>
+            <li>
+                <a href="" class="selected">Home</a>
+            </li>
+            <li>
+                <a href="Resume/resume.html">Resume</a>
+            </li>
+            <li>
+                <a href="Projects/projects.html">Projects</a>
+            </li>
+        </ul>
+    </nav>`
+);
 
-menu.addEventListener('click', (e) => {
 
-    if (nav.classList.contains('responsive')){
-        nav.classList.remove('responsive');
-    } else {
-        nav.classList.add('responsive');
-    }
+
+
+// const menu = document.getElementById("menu");
+// const nav = document.getElementById("nav");
+// console.log("Connected");
+
+// menu.addEventListener('click', (e) => {
+
+//     if (nav.classList.contains('responsive')){
+//         nav.classList.remove('responsive');
+//     } else {
+//         nav.classList.add('responsive');
+//     }
     
-    if (nav.classList.contains('hide')) {
-        nav.classList.remove('hide');
-        nav.classList.add('show');
-    } else {
-        nav.classList.remove('show');
-        nav.classList.add('hide');
-    }
-    console.log("Click");
-});
+//     if (nav.classList.contains('hide')) {
+//         nav.classList.remove('hide');
+//         nav.classList.add('show');
+//     } else {
+//         nav.classList.remove('show');
+//         nav.classList.add('hide');
+//     }
+//     console.log("Click");
+// });
